e79908381d285eca0bd36fa9315b8c99b079c159
Varun Talwar <tallytalwar@gmail.com>
2015-12-23 15:29:25 -0500

    No need to do depth offseting for proxy now

--- core/src/tile/tile.cpp
+++ core/src/tile/tile.cpp
@@ -137,7 +137,8 @@ void Tile::draw(const Style& _style, const View& _view) {
         float zoomAndProxy = m_proxyCounter > 0 ? -m_id.z : m_id.z;
 
         shader->setUniformMatrix4f("u_model", m_modelMatrix);
-        shader->setUniformf("u_tile_origin", m_tileOrigin.x, m_tileOrigin.y, zoomAndProxy);
+        //shader->setUniformf("u_tile_origin", m_tileOrigin.x, m_tileOrigin.y, zoomAndProxy);
+        shader->setUniformf("u_tile_origin", m_tileOrigin.x, m_tileOrigin.y, m_id.z);
 
         styleMesh->draw(*shader);
     }
